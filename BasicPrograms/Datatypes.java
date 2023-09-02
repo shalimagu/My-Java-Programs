6527ec83571688c72da0e07daf1580355f1eddf1
shalimagu <shalima.gu@gmail.com>
2023-09-02 16:31:32 +0530

    Data Type example program

---
+++ BasicPrograms/Datatypes.java
@@ -0,0 +1,25 @@
+package BasicPrograms;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        byte age=36;
+        final String name="May";
+        String address="Whitefield,Bangalore";
+        Boolean IsEligibleToDrive=true;
+        String DOB="12-3-2000";
+       char PassOrFail= 'p';
+       float weight= 50.3f;
+       System.out.println("Students details are as followes   " + "Name:" +name );
+       System.out.println ("Age:"+age );
+        System.out.println("Address:"+
+               address);
+        System.out.println("Date Of Birth:" +DOB);
+        System.out.println("Weight :"+weight);
+        System.out.println("Test Status: "+PassOrFail);
+        System.out.println("Is Eligible for driving:"
+       + IsEligibleToDrive );
+
+
+
+    }
+}
